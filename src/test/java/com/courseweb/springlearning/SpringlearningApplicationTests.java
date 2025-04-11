e6357fd6bde57d24bf0578cead24e3f8a502ff64
Enzo Boadas <enzo.boadas@gmail.com>
2025-04-11 14:55:28 -0300

    Making working tests

--- src/test/java/com/courseweb/springlearning/SpringlearningApplicationTests.java
+++ src/test/java/com/courseweb/springlearning/SpringlearningApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SpringlearningApplicationTests {
 
-	@Test
-	void contextLoads() {
+    @Test
+    void contextLoads() {
 	}
 
 }
